626edebf8371b5d77306279997fefe1501a5208a
kiyo <kiranpro123@gmail.com>
2024-09-07 20:35:02 +0530

    why the fuck do these mfs need message

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import random
+
+class BingoApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title = "Bingo"
+        
+        
+    def create_widgets(self):
+        pass
